862e113f1dbf7182cdca4fd9a5c9d07d5892690f
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2023-03-19 19:07:38 +0300

    Add readme link

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <Main/>
+  <a class="readme" title="Open Readme" href="https://github.com/AlttiRi/href-lister#keeplister" target="_blank"><i>i</i></a>
 </template>
 
 <script setup lang="ts">
@@ -7,5 +8,13 @@ import Main from "./components/Main.vue";
 </script>
 
 <style scoped>
-
+.readme {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 2px 8px;
+  margin: 1px;
+  color: darkgray;
+  text-decoration: none;
+}
 </style>
